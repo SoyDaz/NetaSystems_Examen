ebf523c175c69eae619dca83f4dd1fdccd4dc872
CAPARICIO <cesar.aparicio@landsoft.mx>
2021-06-22 23:30:43 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Android Places
+=================
+
+Una aplicación de lugares que ilustra las mejores prácticas de desarrollo de Android con Android Jetpack.
+
+Introducción
+------------
---
+++ app/src/main/java/com/neta/systems/places/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.neta.systems.places.utils
+
+/**
+ * The class `Constants` represent the global constants of the application.
+ */
+
+class Constants {
+
+    val URL_DEVELOPING: String = ""
+}
