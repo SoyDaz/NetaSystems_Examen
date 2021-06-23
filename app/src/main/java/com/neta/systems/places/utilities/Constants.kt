27005bcf5385a35871eea7eaa12db80831037950
CAPARICIO <cesar.aparicio@landsoft.mx>
2021-06-23 01:24:30 -0500

    Se renombro la carpeta de util

---
+++ app/src/main/java/com/neta/systems/places/utilities/Constants.kt
@@ -0,0 +1,8 @@
+package com.neta.systems.places.utilities
+
+/**
+ * The class `Constants` represent the global constants of the application.
+ */
+
+const val URL_DEVELOPING = ""
+const val DATABASE_NAME = "places-db"
